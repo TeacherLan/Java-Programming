6233ac1433cf2dd6a466ebd91999e7d738c4527c
TeacherLan <losevilleno@pwu.edu.ph>
2026-07-08 15:31:36 +0800

    edit

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("=================================");
-        System.out.println("|JOSE ABAD SANTOS MEMORIAL SCHOOL|");
+        System.out.println("JOSE ABAD SANTOS MEMORIAL SCHOOL");
          System.out.println("=================================");
     }
 }
