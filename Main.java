e02dee267f2184d88ba459ac5830fa584254d0e7
TeacherLan <losevilleno@pwu.edu.ph>
2026-07-08 13:15:01 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    //this is a comment 
+  
+
+    /*  THis is 
+        A Multi line 
+        Comment
+    */
+
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("|JOSE ABAD SANTOS MEMORIAL SCHOOL|");
+         System.out.println("=================================");
+    }
+}
+
+
