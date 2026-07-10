f8dafba0eec8f220c0b710c439fa6a470ed35935
Dexiar <losevilleno@pwu.edu.ph>
2026-07-10 16:39:57 +0800

    Lesson6 - Variables and Data Types

---
+++ Lesson6.java
@@ -0,0 +1,43 @@
+public class Lesson6 {
+ public static void main(String[] args) {
+
+//concatenation
+//    String firstName = "Jace";
+//    String lastName = "Rayos";
+
+//    System.out.println("HI " + firstName + " " + lastName);
+
+    // int x = 5;
+    // int y = 6;
+
+    // System.out.println("The sum is " + x + y); //concatenation
+    // System.out.println("The sum is " + (x + y)); // addition
+
+    // int x = 5;
+    // int y = 6;
+    // int z = 50;
+
+    // x = y = z = 50;
+
+    // System.out.println(x + y + z);
+
+    //constant 
+    // final int BIRTHYEAR = 1999;
+    // final int MINUTES_PER_HOUR = 60;
+    // final double EARTHS_GRAVITY = 9.8;
+
+    // byte number1 = 126; // this is byte
+    // short number2 =  32_767;
+    // int number3 = 1000000000;
+
+    // float number4 = 35e10f;
+    // System.out.println(number4);
+    // double number5 = 12E3d;
+    // System.out.println(number5);
+
+    boolean isRaining = true;
+    char letter = '00';
+    System.out.println(letter);
+ 
+ }
+}
--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=================================");
         System.out.println("JOSE ABAD SANTOS MEMORIAL SCHOOL");
-         System.out.println("=================================");
+        System.out.println("=================================");
     }
 }
 
