7d3e1406797723559d00ee8b5ec89a504929ff41
TeacherLan <losevilleno@pwu.edu.ph>
2026-07-21 15:58:40 +0800

    Java String Methods

---
+++ Lesson8.java
@@ -0,0 +1,50 @@
+public class Lesson8 {
+    public static void main(String[] args) {
+        // string length
+
+        // String alphabets ="Jace Allen";
+        // System.out.println(alphabets.length()); //outputs 10
+
+        //toUpper & toLower
+        // String name = "Emman";
+        // System.out.println(name.toUpperCase());
+        // System.out.println(name.toLowerCase());
+
+        //indexOf()
+        // String name = "Maxinne";
+        // System.out.println((int)name.indexOf("a"));
+
+        // charAt
+        // String name = "Sofia";
+        // System.out.println(name.charAt(0));
+
+        // String name1 = "Rhianne";
+        // String name2 = "Jace";
+        // String name3 = "Rhianne";
+
+        // System.out.println(name1.equals(name2)); name1 == name2
+        // System.out.println(name1.equals(name3)); name1 == name3
+        
+        //trim() && replace()
+        // String name = " Emman pogi 1234 ";
+        // System.out.println(name); // before
+        // System.out.println(name.trim());
+        // System.out.println(name.replace(" ", ""));
+
+        //concatenation
+        // String name = "Sofia";
+        // String description = "The First";
+        // String grade = "11";
+
+        // System.out.println(name + description);
+        // System.out.println(name.concat(description).concat(grade));
+
+        //substring
+        // String name = "Jace Rayos";
+        // System.out.println(name.substring(0,4));
+ 
+        // String name = "Lan \'Dexiar\' Sevilleno";
+        // String name = "Lan\tDexiar Sevilleno";
+        // System.out.println(name);
+    }
+}
